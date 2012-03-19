edc6b1cca67cc67560707beafd4886edb5b9180a
Alex Hayes <alex@alution.com>
2012-03-20 02:52:28 +1100

    Added helper method is_pjax

--- djpjax.py
+++ djpjax.py
@@ -11,7 +11,7 @@ def pjax(pjax_template=None):
             # if not hasattr(resp, "is_rendered"):
             #     warnings.warn("@pjax used with non-template-response view")
             #     return resp
-            if request.META.get('HTTP_X_PJAX', False):
+            if is_pjax(request):
                 if pjax_template:
                     resp.template_name = pjax_template
                 else:
@@ -29,7 +29,7 @@ def pjaxtend(parent='base.html', pjax_parent='pjax.html', context_var='parent'):
             # if not hasattr(resp, "is_rendered"):
             #     warnings.warn("@pjax used with non-template-response view")
             #     return resp
-            if request.META.get('HTTP_X_PJAX', False):
+            if is_pjax(request):
                 resp.context_data[context_var] = pjax_parent
             elif parent:
                 resp.context_data[context_var] = parent
@@ -65,3 +65,10 @@ def _pjaxify_template_name(name):
     else:
         name += "-pjax"
     return name
+
+
+def is_pjax(request):
+    """
+    Returns True if request is considered to be a PJAX request.
+    """
+    return request.META.get('HTTP_X_PJAX', False)
